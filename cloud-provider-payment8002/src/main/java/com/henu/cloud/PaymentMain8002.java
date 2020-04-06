c3c9da0cfc74b79b0bc114e9005e30cc648bcc16
aipl <YEY0929@163.com>
2020-04-06 11:50:20 +0800

    add DisCoveryController

--- cloud-provider-payment8002/src/main/java/com/henu/cloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/henu/cloud/PaymentMain8002.java
@@ -3,12 +3,15 @@ package com.henu.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @MapperScan("com.henu.cloud.dao")
 @SpringBootApplication
 //eureka client
 @EnableEurekaClient
+//允许服务发现
+@EnableDiscoveryClient
 public class PaymentMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8002.class,args);
